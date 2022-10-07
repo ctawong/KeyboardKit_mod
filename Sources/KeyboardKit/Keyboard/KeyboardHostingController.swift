ccdc23cda1c7c12f8b3a97bd04bae2640abfb0e1
Andrew Wong <ctawong@gmail.com>
2022-10-07 16:12:22 -0400

    port fix memory leak

--- Sources/KeyboardKit/Keyboard/KeyboardHostingController.swift
+++ Sources/KeyboardKit/Keyboard/KeyboardHostingController.swift
@@ -36,7 +36,13 @@ public class KeyboardHostingController<Content: View>: UIHostingController<Conte
         view.topAnchor.constraint(equalTo: controller.view.topAnchor).isActive = true
         view.bottomAnchor.constraint(equalTo: controller.view.bottomAnchor).isActive = true
     }
-    
+
+
+
+    deinit {
+        removeFromParent()
+        view.removeFromSuperview()
+    }    
     public override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         updateViewConstraints()
